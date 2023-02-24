e2856c6b2fa242d33e24cd4e63e491849f2eaa4f
jaykwelyn11 <jackie.sepulveda94@gmail.com>
2023-02-23 22:35:30 -0600

    m

--- lectures/cSharp/week2/day2/NewBasicWeb/Controllers/RootController.cs
+++ lectures/cSharp/week2/day2/NewBasicWeb/Controllers/RootController.cs
@@ -1,37 +1,43 @@
 using Microsoft.AspNetCore.Mvc;
 namespace NewBasicWeb.Controllers;
-    public class RootController : Controller
+public class RootController : Controller
+{
+    [HttpGet("")]
+    public ViewResult Index()
     {
-        [HttpGet("")]
-        public ViewResult Index() {
-            return View();
-        }
-       [HttpGet("/fakeRoute")]
-       public RedirectToActionResult Method() {
+        return View();
+    }
+    [HttpGet("/fakeRoute")]
+    public RedirectToActionResult Method()
+    {
         return RedirectToAction("RealRoute");
-       } 
+    }
     //    [HttpGet("/realRoute")]
     //    public ViewResult RealRoute() {
     //     return View("index");
     //    }
-        [HttpGet("/realRoute")]
-       public ViewResult RealRoute() {
+    [HttpGet("/realRoute")]
+    public ViewResult RealRoute()
+    {
         return View();
+    }
+    [HttpGet("/{name}")]
+    public IActionResult Name(string name)
+    {
+        if (name == "melissa")
+        {
+            return RedirectToAction("RealRoute");
         }
-        [HttpGet("/{name}")]
-        public IActionResult Name(string name) {
-            if(name == "melissa") {
-                return RedirectToAction("RealRoute");
-            }
-            else if (name == "juli") {
-                return Json(new {name = name});
-            }
-            // else {
-            //     return $"Hello {name}";
-            // }
-            else {
-                return View("index");
-            }
+        else if (name == "juli")
+        {
+            return Json(new { name = name });
+        }
+        // else {
+        //     return $"Hello {name}";
+        // }
+        else
+        {
+            return View("index");
         }
     }
-
+}
