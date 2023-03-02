353be5ad5885dd139ae6fe3d71f3a0d3a3e74dbc
jaykwelyn11 <jackie.sepulveda94@gmail.com>
2023-03-02 00:21:26 -0600

    DateValidator

--- lectures/cSharp/week3/day3/MoviesWithCRU/MoviesWithCRU/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace MoviesWithCRU.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
